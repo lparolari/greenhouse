af200499187d7c2981fe7909643ca9ce427b7694
lparolari <luca.parolari23@gmail.com>
2021-05-06 19:01:57 +0200

    feat: moisture sensor

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 #include "display.hpp"
 #include "dht.hpp"
 #include "water.hpp"
+#include "brightness.hpp"
+#include "moisture.hpp"
 #include "utils.hpp"
 
 // Sensors:
-// * brightness
+// * [X] brightness
 // * [X] water
-// * moisture
+// * [X] moisture
 // * [X] temperature
 // * [X] humidity
 
@@ -21,6 +23,8 @@
 
 greenhouse::display::Display<> display;
 greenhouse::dht::Dht<> dht;
+greenhouse::moisture::MoistureSensor<1> moisutre_sensor;
+greenhouse::brightness::BrightnessSensor<2> brightness_sensor;
 greenhouse::water::WaterSensor<3> water_sensor;
 
 void setup()
@@ -43,10 +47,15 @@ void loop()
     using greenhouse::utils::eps;
 
     water_sensor.read();
+    brightness_sensor.read();
+    moisutre_sensor.read();
 
-    Serial.println(eps + "Temp " + dht.temperature());
-    Serial.println(eps + "Humi " + dht.relative_humidity());
-    Serial.println(eps + "Water" + water_sensor.get());
+    Serial.println("=======");
+    Serial.println(eps + "Temp = " + dht.temperature());
+    Serial.println(eps + "Humi = " + dht.relative_humidity());
+    Serial.println(eps + "Wate = " + water_sensor.get());
+    Serial.println(eps + "Brig = " + brightness_sensor.get());
+    Serial.println(eps + "Mois = " + moisutre_sensor.get());
 
     delay(1000);
 }
