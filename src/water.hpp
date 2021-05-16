2ee7713e2a53ab02a88e1a9c71b22cd0802d5a38
lparolari <luca.parolari23@gmail.com>
2021-05-16 17:00:39 +0200

    feat: update water sensor with percent sensor

--- src/water.hpp
+++ src/water.hpp
@@ -2,42 +2,17 @@
 
 #include <Arduino.h>
 
+#include "pin.hpp"
+#include "sensor.hpp"
+
 namespace greenhouse::water
 {
-    // @brief An interface to a moisture sensor
-    // @tparam Pin The pin sensor is attached to
-    // @tparam T The measurement type
+
+    // @brief The water sensor
+    // @tparam Pin The (analog) pin number
     // @tparam AirValue The raw value of sensor in dry conditions (in the air)
     // @tparam WaterValue The raw value of sensor in wet conditions (in the water)
-    template <uint8_t Pin = 0U, typename T = float, int AirValue = 250, int WaterValue = 0>
-    class WaterSensor
-    {
-    private:
-        int _value = 0;
+    template <uint8_t Pin = 0U, int AirValue = 250, int WaterValue = 0>
+    using WaterSensor = greenhouse::sensor::PercentSensor<Pin, greenhouse::pin::AnalogInput<Pin>, WaterValue, AirValue, float>;
 
-    public:
-        // @brief Initializes the componet. To be called in Arduino's `setup()` function.
-        void begin()
-        {
-            pinMode(Pin, INPUT);
-        }
-
-        // @brief Reads the raw value
-        void read()
-        {
-            _value = analogRead(Pin);
-        }
-
-        // @returns The percentage of moisture
-        T get() const
-        {
-            return static_cast<T>(_value); // / (LightValue - DarkValue) * 100;
-        }
-
-        // @returns The sensor's raw value
-        int raw() const
-        {
-            return _value;
-        }
-    };
 } // namespace greenhouse::brightness
