0345ca89ec7d085a2e4f0302397d8b09cf5cd409
lparolari <luca.parolari23@gmail.com>
2021-05-16 21:21:36 +0200

    feat: water pump management

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,16 @@
 #include "delay.hpp"
 #include "button.hpp"
 #include "pin.hpp"
+#include "rtc.hpp"
 
+using greenhouse::rtc::DateTime;
 using greenhouse::utils::eps;
 
 const bool debug = true;
 
 greenhouse::display::Display<7, 8, 9, 10, 11, 12> display;
 greenhouse::dht::Dht<2> dht;
-greenhouse::waterpump::WaterPump<23> water_pump;
+greenhouse::waterpump::WaterPump<48> water_pump;
 greenhouse::moisture::MoistureSensor<1> moisutre_sensor;
 greenhouse::brightness::BrightnessSensor<2> brightness_sensor;
 greenhouse::water::WaterSensor<3> water_sensor;
@@ -31,15 +33,30 @@ greenhouse::view::View<5> view;
 greenhouse::delay::Delay<3000> display_delay;
 greenhouse::delay::Delay<1000> debug_delay;
 greenhouse::delay::Delay<100> button_delay;
+greenhouse::delay::Delay<1000> waterpump_delay;
+
+greenhouse::delay::Timer<3000> waterpump_timer;
 
 greenhouse::button::ToggleButton<53> display_slider_button;
 greenhouse::button::DebounceButton<52> slide_button;
 greenhouse::button::DebounceButton<51> waterpump_button;
 greenhouse::button::ToggleButton<50> light_button;
 
-// Many (not functional) functions, better proceudres.
-// This should be changed for program maintainability. However, up to now
+greenhouse::rtc::Rtc<> rtc;
+
+// Many (not so functional) functions which use global state and updates
+// sensors or attuators. We are not so proud of this solution, and we argue
+// that should be changed for program maintainability. However, up to now
 // the only file who knows about components is this one (main.cpp).
+
+// Read digital/analog values from sensors.
+void read_sensors();
+
+// Forward all delays with current millis.
+void forward_delays();
+
+// Create update messages for LCD view.
+void populate_view();
 
 void waterPumpOnToggleHandler();
 void viewScrollToggleHandler();
@@ -62,47 +79,28 @@ void setup()
     slide_button.begin();
     waterpump_button.begin();
     light_button.begin();
+    rtc.begin();
 
     // hello
     greenhouse::serial::hello();
     display.hello();
     dht.sensor_specs();
+
+    // set the alarm for watering the plant at 9 pm
+    rtc.setAlarm(DateTime(0, 0, 0, 21, 19, 15), DS3231_A1_Hour);
 }
 
 void loop()
 {
-    // get value from sensor at every loop, may be delayed
-    // (see sensor's sampling frequency)
-    water_sensor.read();
-    brightness_sensor.read();
-    moisutre_sensor.read();
-
-    // read also button status
-    display_slider_button.read();
-    slide_button.read();
-    waterpump_button.read();
-    light_button.read();
-
-    // forward delays
-    auto ms = millis();
-
-    display_delay.tick(ms);
-    button_delay.tick(ms);
-    debug_delay.tick(ms);
-
-    // prepare the view with the display
-    view.clear();
-    view.add_message(eps + "Temp = " + dht.temperature() + " %");
-    view.add_message(eps + "Humi = " + dht.relative_humidity() + " %");
-    view.add_message(eps + "Wate = " + water_sensor.get() + " %" + " (" + water_sensor.raw() + ")");
-    view.add_message(eps + "Brig = " + brightness_sensor.get() + " %" + " (" + brightness_sensor.raw() + ")");
-    view.add_message(eps + "Mois = " + moisutre_sensor.get() + " %" + " (" + moisutre_sensor.raw() + ")");
+    read_sensors();
+    forward_delays();
 
     // trigger delayed actions for display
     if (display_delay.is_fire())
     {
         display_delay.fired();
 
+        populate_view();
         viewScrollToggleHandler();
 
         display.display(view.get_message());
@@ -116,7 +114,7 @@ void loop()
         // A delay may not be needed in this case, however
         // we want to avoid noise (which also buttons handle).
 
-        waterPumpOnToggleHandler();
+        // waterPumpOnToggleHandler();
         viewSliderToggleHandler();
         lightToggleHandler();
     }
@@ -128,17 +126,71 @@ void loop()
         printDebugSerial();
     }
 
+    // trigger watering
+    if (waterpump_delay.is_fire())
+    {
+        if (rtc.alarmFired())
+        {
+            waterpump_timer.tick(millis());
+
+            water_pump.on();
+
+            if (waterpump_timer.is_after())
+            {
+                water_pump.off();
+                rtc.clearAlarm();
+            }
+        }
+
+        waterpump_delay.fired();
+    }
+
     // wait 1 ms, prevent oversampling [without source]
     delay(1);
 }
 
-void waterPumpOnToggleHandler()
+void read_sensors()
 {
-    if (waterpump_button.pressed())
-        water_pump.on();
-    else
-        water_pump.off();
+    // get value from sensor at every loop, may be delayed
+    // (see sensor's sampling frequency)
+    water_sensor.read();
+    brightness_sensor.read();
+    moisutre_sensor.read();
+
+    // read also button status
+    display_slider_button.read();
+    slide_button.read();
+    waterpump_button.read();
+    light_button.read();
 }
+
+void forward_delays()
+{
+    auto ms = millis();
+
+    display_delay.tick(ms);
+    button_delay.tick(ms);
+    debug_delay.tick(ms);
+    waterpump_delay.tick(ms);
+}
+
+void populate_view()
+{
+    view.clear();
+    view.add_message(eps + "Temp = " + dht.temperature() + " %");
+    view.add_message(eps + "Humi = " + dht.relative_humidity() + " %");
+    view.add_message(eps + "Wate = " + water_sensor.get() + " %" + " (" + water_sensor.raw() + ")");
+    view.add_message(eps + "Brig = " + brightness_sensor.get() + " %" + " (" + brightness_sensor.raw() + ")");
+    view.add_message(eps + "Mois = " + moisutre_sensor.get() + " %" + " (" + moisutre_sensor.raw() + ")");
+}
+
+// void waterPumpOnToggleHandler()
+// {
+//     if (waterpump_button.pressed())
+//         water_pump.on();
+//     else
+//         water_pump.off();
+// }
 
 void viewScrollToggleHandler()
 {
@@ -176,4 +228,5 @@ void printDebugSerial()
     Serial.println(eps + "Next = " + slide_button.pressed());
     Serial.println(eps + "Wate = " + waterpump_button.pressed());
     Serial.println(eps + "Ligh = " + light_button.pressed());
+    Serial.println(eps + "Time = " + rtc.now().hour() + ":" + rtc.now().minute() + ":" + rtc.now().second());
 }
